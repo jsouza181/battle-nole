0ccc7015032b5ac7d153529805bcd4016d63ca98
jsouza181 <jsouza181@yahoo.com>
2015-07-11 18:32:26 -0400

    Initial commit.

---
+++ app/src/main/java/com/electricsheep/battlenole/BattleNole.java
@@ -0,0 +1,26 @@
+package com.electricsheep.battlenole;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/*
+ * BattleNole is the main/launcher activity for Battle 'Nole.
+ * It displays the title/artwork of the game and the main menu, providing the player with the
+ * ability to:
+ *   -Start a new game
+ *   -Adjust settings
+ *   -More later?
+ *
+ * Version 0.1.0 - July 11, 2015
+ * Jason Souza
+ */
+public class BattleNole extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle_nole);
+    }
+
+}
